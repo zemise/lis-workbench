de7cd7e6e7102004d80b420e1fa24535d229c6f9
zemise <zhaoxiaowang@outlook.com>
2026-05-04 20:36:30 +0800

    fix: compact bottom button bar height and margins

--- src_qt/main_window.cpp
+++ src_qt/main_window.cpp
@@ -83,11 +83,13 @@ void MainWindow::setupUi() {
 
     // Bottom bar
     auto* bottom = new QWidget;
+    bottom->setFixedHeight(36);
     setupButtonBar(bottom);
 
     // Main layout
     auto* mainLayout = new QVBoxLayout(central);
-    mainLayout->setContentsMargins(6, 6, 6, 6);
+    mainLayout->setContentsMargins(4, 4, 4, 0);
+    mainLayout->setSpacing(2);
     mainLayout->addWidget(splitter_);
     mainLayout->addWidget(bottom);
 
@@ -205,11 +207,12 @@ void MainWindow::setupResultTable() {
 
 void MainWindow::setupButtonBar(QWidget* panel) {
     auto* layout = new QHBoxLayout(panel);
-    layout->setContentsMargins(0, 4, 0, 0);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(4);
 
     auto makeBtn = [&](const QString& text) {
         auto* btn = new QPushButton(text);
-        btn->setFixedSize(92, 32);
+        btn->setFixedSize(88, 28);
         return btn;
     };
 
