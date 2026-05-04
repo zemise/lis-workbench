92787fb49953e8817dda503ee2a47856012d0753
zemise <zhaoxiaowang@outlook.com>
2026-05-04 22:27:39 +0800

    feat: embed app icon via Qt resource system for all windows

--- src_qt/main.cpp
+++ src_qt/main.cpp
@@ -1,11 +1,13 @@
 #include "main_window.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     app.setApplicationName("ResultSearch");
     app.setApplicationDisplayName(QString::fromWCharArray(L"检验结果查询"));
+    app.setWindowIcon(QIcon(":/app.ico"));
 
     MainWindow window;
     window.show();
